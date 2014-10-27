aa62ceb07dff061f033dbbb6ecd4e49509b143fa
Egor Vinogradov <egorvinogradov@live.ru>
2014-10-26 20:59:59 -0700

    updated add_icon.sh

--- add_icon.sh
+++ add_icon.sh
@@ -11,16 +11,26 @@ filename=$(basename "$file")
 extension="${filename##*.}"
 filename="${filename%.*}"
 
-link_order=$project_root/apps/ah/static/js/link-order.coffee
+link_order_app=$project_root/apps/ah/static/js/link-order.coffee
+link_order_extension=$project_root/extension/chrome/js/link-order.coffee
+
 extension_ui_path=$project_root/extension/chrome/css/
 interface_ui_path=$project_root/apps/ah/static/css/modules/
 
-cp $link_order _temp
-sed '$ d' _temp > $link_order
+cp $link_order_app _temp
+sed '$ d' _temp > $link_order_app
 rm -f _temp
 
 echo "    \"$filename\"
-]" >> $link_order
+]" >> $link_order_app
+
+cp $link_order_extension _temp
+sed '$ d' _temp > $link_order_extension
+rm -f _temp
+
+echo "    \"$filename\"
+]" >> $link_order_extension
+
 
 echo "\n.ah-icon-site_$filename\n    background-image: url('chrome-extension://__MSG_@@extension_id__/img/favicons/$filename.$extension')" >> $extension_ui_path/ui.sass
 
