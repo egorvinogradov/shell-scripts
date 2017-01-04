e088c06b4b8845518a8cb186dc1a22a4b515ae8e
Egor Vinogradov <egorvinogradov@live.ru>
2017-01-04 15:33:34 -0800

    Add dictation language change script

---
+++ dictation_change_language.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+k="com.apple.speech.recognition.AppleSpeechRecognition.prefs DictationIMLocaleIdentifier"
+if [[ "$(defaults read $k)" == en-US ]]; then
+  defaults write $k ru-RU
+  defaults write com.apple.assistant "Session Language" ru-RU
+else
+  defaults write $k en-US 
+  defaults write com.apple.assistant "Session Language" en-US
+fi
+killall -HUP DictationIM
